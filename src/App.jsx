79760343ff728ebaa40c1ddddd906917cdf5c0da
mathildeew <mathildewiik@hotmail.com>
2023-05-22 15:15:02 +0200

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,92 @@
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+function Nav() {
+  return <nav></nav>;
+}
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+    </header>
+  );
+}
+
+function Footer() {
+  return <footer>THis is footer</footer>;
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Product() {
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
+}
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  );
+}
+
+function CheckoutSuccess() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+}
+
+function RouteNotFound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
 export default function App() {
-  return;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="cart" element={<Cart />}></Route>
+          <Route path="checkoutsuccess" element={<CheckoutSuccess />}></Route>
+          <Route path="contactus" element={<Contact />}></Route>
+          <Route path="*" element={<RouteNotFound />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
