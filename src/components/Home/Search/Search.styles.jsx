b6cfcda3048e20a648fc3f81acee35025d70f4df
mathildeew <mathildewiik@hotmail.com>
2023-05-23 20:32:03 +0200

    remove X from search input

--- src/components/Home/Search/Search.styles.jsx
+++ src/components/Home/Search/Search.styles.jsx
@@ -15,4 +15,11 @@ export const SearchInput = styled.div`
     padding: 10px;
     width: 100%;
   }
+
+  input[type="search"]::-webkit-search-cancel-button,
+  input[type="search"]::-webkit-search-decoration,
+  input[type="search"]::-webkit-search-results-button,
+  input[type="search"]::-webkit-search-results-decoration {
+    display: none;
+  }
 `;
