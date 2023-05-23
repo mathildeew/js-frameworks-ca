85a9c08b7ce3ed799ae4e5702f125e126f6812a9
mathildeew <mathildewiik@hotmail.com>
2023-05-23 20:28:53 +0200

    adds on search function

--- src/components/Home/Search/index.jsx
+++ src/components/Home/Search/index.jsx
@@ -1,11 +1,30 @@
 import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { fetchApi } from "../FetchApi";
 import { SearchInput } from "./Search.styles";
 
 export default function Search() {
+  const { products } = fetchApi("https://api.noroff.dev/api/v1/online-shop");
+
+  const [searchedProduct, setSearchedProduct] = useState("");
+
+  function onSearch(event) {
+    setSearchedProduct(event.target.value);
+
+    const results = products.filter((product) => {
+      if (event.target.value === "") return products;
+      return product.title
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
+    });
+
+    console.log(results);
+  }
+
   return (
     <SearchInput>
-      <input placeholder="Search product" />
+      <input placeholder="Search product" type="search" onChange={onSearch} />
       <FontAwesomeIcon icon={Icons.faMagnifyingGlass} />
     </SearchInput>
   );
