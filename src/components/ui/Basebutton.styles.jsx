684528980c5d17f586c087d2272a15caafb09fa9
mathildeew <mathildewiik@hotmail.com>
2023-06-04 19:14:10 +0200

    new styles for basebutton

--- src/components/ui/Basebutton.styles.jsx
+++ src/components/ui/Basebutton.styles.jsx
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 
 export const BaseButton = styled.button`
-  color: var(--color-primary);
+  color: var(--color-white);
   font-size: 14px;
   font-weight: 600;
   text-transform: uppercase;
-  background-color: var(--color-secondary);
+  background-color: var(--color-primary);
   border-style: none;
   padding-top: 8px;
   padding-bottom: 8px;
   margin-bottom: 20px;
 
-  background-color: ${(props) => (props.isWhite ? "var(--color-primary)" : "")};
-  border: ${(props) =>
-    props.isWhite ? "2px solid var(--color-secondary)" : ""};
+  background-color: ${(props) => (props.isWhite ? "var(--color-white)" : "")};
+  border: ${(props) => (props.isWhite ? "2px solid var(--color-primary)" : "")};
 
   a {
     color: var(--color-primary);
-    color: ${(props) => (props.isWhite ? "var(--color-secondary)" : "")};
+    color: ${(props) => (props.isWhite ? "var(--color-primary)" : "")};
   }
 `;
